354c69ebd4c9f18d0bec8f2a2180094c9c2bd3d6
aspeedi777 <aspeedi777@gmail.com>
2020-08-24 21:18:20 +0200

    project setup

---
+++ src/main/java/com/pl/arkadiusz/diet_pro/DietProApplication.java
@@ -0,0 +1,13 @@
+package com.pl.arkadiusz.diet_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DietProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pl/arkadiusz/diet_pro/DietProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.arkadiusz.diet_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
