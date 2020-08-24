495d169908cead198fea1fbf9335f0cfa32cac01
aspeedi777 <aspeedi777@gmail.com>
2020-08-24 21:25:36 +0200

    configure H2 jpa and JDBC  in properties, and add odel mapper to projesct as Bean

---
+++ src/main/java/com/pl/arkadiusz/diet_pro/config/BeansConfiguration.java
@@ -0,0 +1,17 @@
+package com.pl.arkadiusz.diet_pro.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeansConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
+    }
+
+}
