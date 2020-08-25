243fba7dbcd40add727d02eca8a17d30c9fd7e14
aspeedi777 <aspeedi777@gmail.com>
2020-08-25 15:07:38 +0200

    conect intelijei with h2 database repair

--- src/main/java/com/pl/arkadiusz/diet_pro/DietProApplication.java
+++ src/main/java/com/pl/arkadiusz/diet_pro/DietProApplication.java
@@ -1,40 +1,18 @@
 package com.pl.arkadiusz.diet_pro;
-
-import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.sql.SQLException;
+
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"com.pl.arkadiusz.diet_pro"})
-public class DietProApplication extends SpringBootServletInitializer {
+public class DietProApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(DietProApplication.class, args);
     }
 
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        startH2Server();
-        return application.sources(DietProApplication.class);
-    }
-
-    private static void startH2Server() {
-        try {
-            Server h2Server = Server.createTcpServer().start();
-            if (h2Server.isRunning(true)) {
-
-            } else {
-                throw new RuntimeException("Could not start H2 server.");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Failed to start H2 server: ", e);
-        }
-    }
 
 }
